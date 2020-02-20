26f68c2bbe6d94f1c9509ef7c87cb1f0ac762101
Vivica Ma <vivicacongma@gmail.com>
2020-02-20 21:30:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,7 +43,7 @@ while director_index < nds.length do
   movie_index = 0 
   
   while movie_index < nds[director_index][:movies].length do 
-    result = [director_name][director_index] += nds[director_index][:movies][movie_index][:worldwide_gross]
+    result = [director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
     movie_index += 1 
   end 
     director_index += 1 
